f2a0ec63d7d9f3f1d898564196a05bdc44e362ef
fsaito <Fumio.Saito@sony.com>
2018-03-28 16:25:52 +0900

    page3

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -28,3 +28,4 @@ const App = (props: AppProps) => {
   );
 };
 export default App;
+
--- src/components/Pages.tsx
+++ src/components/Pages.tsx
@@ -5,14 +5,15 @@ import { MaterialUis } from './MaterialUis';
 
 export const
     Contact = () => <div><h2>Contact</h2>
+    <div>
+        submit後に、reduxのactionが起動している。
+        <a href="https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd?utm_source=chrome-ntp-icon">公式拡張で見ると面白いよ</a>
+    </div>
     <div><FirstForm /></div>
     </div>
 
 export const
     About = () => <div><h2>About</h2>
-    <div>
-        submit後に、reduxのactionが起動している。
-        <a href="https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd?utm_source=chrome-ntp-icon">公式拡張で見ると面白いよ</a>
-    </div>
+
     <MaterialUis />
     </div>

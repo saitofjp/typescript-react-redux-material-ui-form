05b7c972eba54ccc2286465980f1c1c6f34d3c5c
saitofjp <saitofjp@gmail.com>
2018-03-30 02:18:54 +0900

    add models

--- src/selectors/sample3.ts
+++ src/selectors/sample3.ts
@@ -1,6 +1,8 @@
 import { createSelector } from 'reselect';
 import { State } from "../reducers";
 
+//モデルの場合、再計算コスト削減のreselect必須
+
 export const smaple3Selector = (state:State) => state.sample3;
 //CQRSのQuery
 export const isListFetched = createSelector(smaple3Selector, (sample3) => sample3.isFetched);

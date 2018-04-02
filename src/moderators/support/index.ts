4c7578fbb1d39cf7dc61080d2d9c7313ceaa41b6
saitofjp <saitofjp@gmail.com>
2018-04-02 22:29:45 +0900

    chg type

--- src/moderators/support/index.ts
+++ src/moderators/support/index.ts
@@ -9,10 +9,10 @@ export type ThunkAction<R> = ThunkAction<R, State, void>;
 
 export const asyncActionTemplate = <PARAMS, RESULT>(
     asyncAction: AsyncActionCreators<PARAMS, RESULT, Error>,
-    inner: (p:PARAMS) => ThunkAction<Promise<RESULT|undefined> , State ,any >,
-    loading: ((now:boolean) => Action) | false  = (now) => loadingAction(now)
+    inner: (p: PARAMS) => ThunkAction<Promise<RESULT | undefined>, State, any>,
+    loading: ((now: boolean) => Action) | false = (now) => loadingAction(now)
 ) => {
-    return (paramsArg?: PARAMS) => {
+    return (paramsArg?: PARAMS): ThunkAction<Promise<void>, State, any> => {
         const params = paramsArg ? paramsArg : {} as PARAMS;
         return async (dispatch: Dispatch<State>, getState: () => State) => {
             try {

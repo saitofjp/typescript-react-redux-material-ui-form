bc3afbe2096dfbf2710f859669a38a7f1ab74fa5
saitofjp <saitofjp@gmail.com>
2018-04-02 22:36:54 +0900

    chg filename

--- src/moderators/index.ts
+++ src/moderators/index.ts
@@ -1,5 +1,5 @@
-import {sample3Binder} from "./sample3.moderator"
-import { environmentBinder } from "./environment.moderator";
+import {sample3Binder} from "./sample3"
+import { environmentBinder } from "./environment";
 import { bindActionDispatcherCreater } from "./support/bindActionDispacher";
 
 export const bindActionDispatcher = bindActionDispatcherCreater([
